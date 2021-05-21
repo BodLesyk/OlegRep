eb543acb0f97c71ec8e91134ae6d017d6d239f50
Oscar Dylan <o.dylan@creatio.com>
2021-05-21 12:55:07 +0300

    First Commit

---
+++ AuthenticationRequest/Program.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Xml;
+using System.Net;
+using System.IO;
+
+namespace AuthenticationRequest
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            JSONRequest requestTest = new JSONRequest();
+            requestTest.TryLogin();
+            requestTest.formRequestData();
+            requestTest.createNewTestJSONRecord();
+            Console.ReadKey();
+
+        }
+    }
+}
